df12ee1eaf83a266218d9a67b68111ef67b23235
Muazzam Bin Aqeel <128594910+muazzamaqeel@users.noreply.github.com>
2024-03-23 17:08:24 +0100

    Add. Ex 1.5 PartB

--- University/Assingment 1/src/assignment01/Exercise1_5_Part_B.java
+++ University/Assingment 1/src/assignment01/Exercise1_5_Part_B.java
@@ -1,22 +1,26 @@
 package assignment01;
-public class Exercise1_5_Part_A {
+
+public class Exercise1_5_Part_B {
+
     public static int i = 0;
     public static boolean containsOdd(int[] arr, int endIndex){
         if(arr==null || endIndex >= arr.length){
-            return false;
+            return true;
         }else {
-            if (arr[endIndex] % 2 == 0) {
+            if (arr[endIndex] % 2 != 0) {  //Odd
                 return containsOdd(arr,endIndex+1);
+            }
+            if (arr[endIndex] % 2 == 0) { //Even
+                return false;
             }
             return true;
         }
     }
     public static void main(String[] args) {
 
-        int[] arr = {3,4,2};
+        int[] arr = {3,13,7,2,2,2,2,41,2,3};
         int endIndex = 0;
         boolean Result = containsOdd(arr, endIndex);
-        System.out.println("Result: " +Result);
-
+        System.out.println("All the values are odd: " + Result);
     }
 }
