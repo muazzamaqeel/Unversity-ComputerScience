06a9920f876e7ab84b5a83d5dbed7fe6c9f3d541
Muazzam Bin Aqeel <128594910+muazzamaqeel@users.noreply.github.com>
2024-04-01 17:19:56 +0200

    Update Main.java

--- Algorithms and Data Structure/Sorting-Algorithms/BubbleSort/src/Main.java
+++ Algorithms and Data Structure/Sorting-Algorithms/BubbleSort/src/Main.java
@@ -8,7 +8,6 @@ public class Main {
             System.out.print(i + " ");
         }
     }
-
     public static void bubbleSort(int[] array) {
         for (int i = 0; i < array.length - 1; i++) {
             for (int j = 0; j < array.length - i - 1; j++) {
