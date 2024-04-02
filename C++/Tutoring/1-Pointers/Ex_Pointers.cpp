98971d1d9890fd585dcfd5d4a0e703b6b6e66a88
Muazzam Bin Aqeel <128594910+muazzamaqeel@users.noreply.github.com>
2024-04-02 13:31:41 +0200

    Pointer Worksheet

--- C++/Tutoring/1-Pointers/Ex_Pointers.cpp
+++ C++/Tutoring/1-Pointers/Ex_Pointers.cpp
@@ -39,8 +39,46 @@ There are also two integer pointers named ptrA and ptrB. Assign the values of 'a
     increment(&num); // Passing address of 'num' to the function
     std::cout << "After incrementing: " << num << std::endl;
 
+
+//Exercise 4
+
+	int temp = 20; 
+    int* ptr = new int(temp); // 'new' allocates memory for an integer and initializes it to 10
+    std::cout << "Value of *ptr: " << *ptr << std::endl; // Access the value at the memory address stored in ptr
+
+	int temp1 = 30; 
+    int* ptr1 = new int();
+    ptr1 = &temp1;
+	std::cout << "Value of *ptr1: " << *ptr1 << std::endl;
+
+
 	return 0;
 
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    delete ptr; // Free the memory allocated for ptr
+  //  ptr = nullptr; // It's a good practice to set the pointer to nullptr after deleting its heap memory
 
