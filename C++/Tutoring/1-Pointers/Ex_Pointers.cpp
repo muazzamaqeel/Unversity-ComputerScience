df5ed86b8ed8ced6a5e36d80b06ab05c6f4a0ed7
Muazzam Bin Aqeel <128594910+muazzamaqeel@users.noreply.github.com>
2024-04-02 13:38:35 +0200

    Fix

--- C++/Tutoring/1-Pointers/Ex_Pointers.cpp
+++ C++/Tutoring/1-Pointers/Ex_Pointers.cpp
@@ -4,6 +4,13 @@
 #include <iostream>
 using namespace std;
 
+/*Exercise 1
+Write a program that asks the user to enter integers as inputs to be stored in the variables 'a' and 'b' respectively.
+There are also two integer pointers named ptrA and ptrB. Assign the values of 'a' and 'b' to ptrA and ptrB respectively, and display them
+*/
+
+
+
 
 
 
@@ -12,32 +19,29 @@ void increment(int* ptr) {
 	//*ptr=*ptr+1;
 	
 }
-
-
 int main(){
 
-/*Exercise 1
-Write a program that asks the user to enter integers as inputs to be stored in the variables 'a' and 'b' respectively.
-There are also two integer pointers named ptrA and ptrB. Assign the values of 'a' and 'b' to ptrA and ptrB respectively, and display them
-*/
-
-
 //Exercise 2
-	char str[] = "A string.";
-	char *pc = str;
+	int num = 20;
+	int *pc1;
+    std::cout << "Before incrementing: " << num << std::endl;
+    increment(pc1); // Passing address of 'num' to the function
+    std::cout << "After incrementing: " << num << std::endl;
 
-	cout << str[0] <<' '<< *pc <<' '<<pc[3]<<"\n";
-	
-	pc =pc+3;
-	
-	cout <<*pc<<' '<< pc[2] <<' '<< pc[5];
+
 
 
 //Exercise 3
-	int num = 10;
-    std::cout << "Before incrementing: " << num << std::endl;
-    increment(&num); // Passing address of 'num' to the function
-    std::cout << "After incrementing: " << num << std::endl;
+	char str[] = "A string.";
+	char *pc = str;
+
+	//cout << str[0] <<' '<< *pc <<' '<<pc[3]<<"\n";
+	
+	pc =pc+3;
+	
+	//cout <<*pc<<' '<< pc[2] <<' '<< pc[5];
+
+
 
 
 //Exercise 4
