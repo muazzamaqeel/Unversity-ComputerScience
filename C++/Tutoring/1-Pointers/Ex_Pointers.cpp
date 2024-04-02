c36268457020ddd1a14af3afb043dda746a5ccd0
Muazzam Bin Aqeel <128594910+muazzamaqeel@users.noreply.github.com>
2024-04-02 23:27:45 +0200

    Fix

--- C++/Tutoring/1-Pointers/Ex_Pointers.cpp
+++ C++/Tutoring/1-Pointers/Ex_Pointers.cpp
@@ -14,29 +14,24 @@ void increment(int* ptr) {
 
 int main(){
 
+/////////////////////////////////////////////////////Weekly Ex./////////////////////////////////////////////////////
 
 /*Exercise 1
 Write a program that asks the user to enter integers as inputs to be stored in the variables 'a' and 'b' respectively.
 There are also two integer pointers named ptrA and ptrB. Assign the values of 'a' and 'b' to ptrA and ptrB respectively, and display them
 */
 
+//Exercise 2: Write a program in C++ to add two numbers using pointers.
 
+//Exercise 3: Write a program in C++ to store n elements in an array and print the elements using a pointer.
 
-//Exercise 2: Write a program in C to add two numbers using pointers.
-
-
-//Exercise 3: Write a program in C to store n elements in an array and print the elements using a pointer.
-
-
-//Exercise 4: Write a program in C to calculate the length of a string using a pointer.
-
+//Exercise 4: Write a program in C++ to calculate the length of a string using a pointer.
 
 /* Exercise 5:
 Allocate two pointers on the heap. Each pointer should reference its own variable.
 Pass these pointers to a function. The function's task is to determine which pointer refers to a variable with the larger value.
 After the function execution is complete, deallocate the memory for both pointers on the heap to prevent memory leaks.
 */
-
 
 /*Exercise 6:
 	Show an code example for the following concepts (Feel free to play around - There are no boundries for this question)
@@ -46,15 +41,12 @@ After the function execution is complete, deallocate the memory for both pointer
 
 */
 
-
-
 //Advance
 //Exercise 1. 
-//Write a program in C to sort an array using a pointer.
-
+//Write a program in C++ to sort an array using a pointer.
 
 //Exercise 2. 
-//Write a C program to demonstrate how a function returns a pointer.
+//Write a C++ program to demonstrate how a function returns a pointer.
 
 /*
 Exercise 3.
