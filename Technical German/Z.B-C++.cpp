649ab9ab228b4d68fdb159e479bada1f0a198cd3
Muazzam Bin Aqeel <128594910+muazzamaqeel@users.noreply.github.com>
2024-04-20 16:36:22 +0200

    Tech. German

---
+++ Technical German/Z.B-C++.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    int a=1;
+    int b=2;
+    int c=a+b;
+    cout<<c<<endl;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//cout<<"Ich heiße Muazzam"<<endl;
---
+++ Technical German/Z.B.Python.py
@@ -0,0 +1,5 @@
+print("Ich heiße Muazzam")
+a=1
+b=2
+c=a+b
+print("a+b=",c)
