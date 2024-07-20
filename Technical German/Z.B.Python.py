5794e3d0996f7d8e505cdb563d2369bcfb028d69
muazzamaqeel <aqeemu01@thu.de>
2024-07-20 14:34:35 +0200

    Structure

--- Technical German/Z.B.Python.py
+++
@@ -1,5 +0,0 @@
-print("Ich heiße Muazzam")
-a=1
-b=2
-c=a+b
-print("a+b=",c)
--- Technical German/ZBJava.java
+++
@@ -1,8 +0,0 @@
-public class ZBJava {
-    public static void main(String[] args) {
-        int a=10;
-        int b=2;
-        int c=a+b;
-        System.out.println(c);
-    }
-}
