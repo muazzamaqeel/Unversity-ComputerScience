90f686fc37b0c542e08f0c5a0fb2aefb8989ce04
muazzamaqeel <aqeemu01@thu.de>
2024-11-21 10:29:43 +0100

    Lecture

---
+++ C++/Tutoring/Lectures/WS-24/Classes & Objects/Code/Test.h
@@ -0,0 +1,19 @@
+//
+// Created by muazz on 24/10/2024.
+//
+
+#ifndef CODE_TEST_H
+#define CODE_TEST_H
+
+class Test {
+public:
+    Test();              // Constructor
+    void setValue(int val);  // Setter
+    int getValue() const;    // Getter
+
+private:
+    int value;               // Private member variable
+};
+
+
+#endif //CODE_TEST_H
