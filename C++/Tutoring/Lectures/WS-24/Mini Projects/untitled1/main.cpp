930dd6cca2747c2d7868c6ca695705c10342b80d
muazzamaqeel <aqeemu01@thu.de>
2024-12-11 09:42:59 +0100

    Updates

--- C++/Tutoring/Lectures/WS-24/Mini Projects/untitled1/main.cpp
+++ C++/Tutoring/Lectures/WS-24/Mini Projects/untitled1/main.cpp
@@ -1,48 +1,66 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-class Point{
 
+//Abstract Class
+//We can't make an object from this class
+class Vehicle {
 public:
-    int x,y;
+    string name;
+    Vehicle(const std::string& name) : name(name) {}
+    //Virtual Function is always = 0
+    virtual double getFuelEfficiency()  = 0;
+    friend ostream& operator<<(ostream& os,Vehicle& vehicle){
+        os << "Vehicle Name: " << vehicle.name<<"\n"
+           << "Fuel Efficiency: " << vehicle.getFuelEfficiency() << "km/l"<<endl;
+           return os;
+    }
+    virtual ~Vehicle(){
+        cout <<"Destructor of Vehicle has been called"<<endl;
+    }
+};
+class Car : public Vehicle{
+public:
+    double engineSize;
+    double weight;
 
-    //Constructor
-    Point(int x1=0, int y1=0):x(x1),y(y1){
+    Car(string name, double engineSize, double weight):Vehicle(name),engineSize(engineSize),weight(weight){}
+
+    double getFuelEfficiency() override {
+        return 100.0 / (engineSize * (weight / 1000.0));
     }
 
-    Point operator+(Point& other){
-        Point temp;
-        //x is what p1 is in the main fucntion
-        temp.x = x + other.x;
-        temp.y = y + other.y;
-        return temp;
+};
+class Bike : public Vehicle{
+public:
+    double engineCC;
+
+    Bike(string name, double engineCC): Vehicle(name), engineCC(engineCC) {}
+
+    double getFuelEfficiency() override {
+        double FuelEfficiency;
+        FuelEfficiency = 200/engineCC;
+        return FuelEfficiency;
 
     }
-    Point operator-(Point& other){
-        Point temp;
-        //x is what p1 is in the main fucntion
-        temp.x = x - other.x;
-        temp.y = y - other.y;
-        return temp;
-    }
-    // Overloads the << operator to print Point objects.
-    // The reason we need to have this function as a friend becasue LHS is ostream and its not a member of Point class
-
-     friend ostream& operator<<(ostream& os, Point& point){
-        os << "(" << point.x << " Muazzam "<< point.y << ")";
-        return os;
-     }
 
 };
 
 
 int main() {
-    //Point *p1=new Point();
-    Point p1(3,4);
-    Point p2(1,2);
 
-    Point p3=p1+p2;
-    Point p4=p1-p2;
+    //Vehicle*v1=new Car("Mazda",2.0, 1500.0);
 
-    cout<<p3<<endl;
+    vector<Vehicle*> vehicles;
+    vehicles.push_back(new Car("Mazda",2.0, 1500.0));
+    vehicles.push_back(new Bike("Motorbike", 150));
+
+    for(int i = 0; i < vehicles.size(); i++){
+        cout<<*vehicles[i];
+    }
+    for(int i = 0; i < vehicles.size(); i++) {
+        delete vehicles[i];
+    }
+
 }
